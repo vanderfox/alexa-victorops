54e5cb36cded8fa4c061fafcac47cae943e84989
lfox <lee.h.fox@gmail.com>
2017-02-20 00:28:04 -0600

    last fix on welcome message

--- grails-app/speechlets/com/vanderfox/VictorOPSSpeechlet.groovy
+++ grails-app/speechlets/com/vanderfox/VictorOPSSpeechlet.groovy
@@ -244,7 +244,7 @@ class VictorOPSSpeechlet implements GrailsConfigurationAware, Speechlet {
     }
 
     SpeechletResponse getWelcomeResponse()  {
-        String speechText = "<speak>Welcome to the <phoneme alphabet='ipa' ph='vɪktorɔps'>victorops</phoneme> skill - say List Open Incidents for open incidents</speak>"
+        String speechText = "Welcome to the victorops skill - say List Open Incidents for open incidents"
 
         // Create the Simple card content.
         SimpleCard card = new SimpleCard(title: "VictorOPS", content: speechText)
